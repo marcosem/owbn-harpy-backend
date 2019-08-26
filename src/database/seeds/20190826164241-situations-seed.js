c963400f6c25ca638f1a0b61410b4a50b2b41cdf
Marcos Mathias <marcos-github@memathias.com>
2019-08-26 13:45:25 -0300

    Added Situations Seed

---
+++ src/database/seeds/20190826164241-situations-seed.js
@@ -0,0 +1,50 @@
+module.exports = {
+  up: queryInterface => {
+    return queryInterface.bulkInsert(
+      'situations',
+      [
+        {
+          situation_en: 'Active',
+          situation_pt: 'Ativo',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          situation_en: 'Unknow',
+          situation_pt: 'Desconhecida',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          situation_en: 'Destroyed',
+          situation_pt: 'Destruído',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          situation_en: 'Torpor',
+          situation_pt: 'Torpor',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          situation_en: 'Staked',
+          situation_pt: 'Estacado',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          situation_en: 'Inactive',
+          situation_pt: 'Inativo',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: queryInterface => {
+    return queryInterface.bulkDelete('situations', null, {});
+  },
+};
